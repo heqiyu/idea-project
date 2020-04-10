b4014860840e3039c2b04a3c5cdc9f356ccec974
zhangsan <zhangsan@163.com>
2020-04-10 23:21:07 +0800

    将项目提交到Git本地仓库

---
+++ src/com/itcast/git/git.java
@@ -0,0 +1,12 @@
+package com.itcast.git;
+
+public class git {
+    public void main(String[] args){
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
